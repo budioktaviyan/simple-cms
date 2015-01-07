fc9d55db8e1c6bf32f1d4402470b7b497d57059c
Budi Oktaviyan Suryanto <budi.oktaviyan@icloud.com>
2015-01-07 20:28:37 +0800

    Update table role for user

--- src/main/resources/application.sql
+++ src/main/resources/application.sql
@@ -14,9 +14,21 @@ CREATE TABLE `tbl_employee` (
 
 DROP TABLE IF EXISTS `tbl_user`;
 CREATE TABLE `tbl_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`username`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO tbl_user values('admin', SHA('admin'));
+DROP TABLE IF EXISTS `tbl_role`;
+CREATE TABLE `tbl_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role` varchar(255) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_USER` (`user_id`),
+  CONSTRAINT `FK_USER` FOREIGN KEY (`user_id`) REFERENCES `tbl_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO tbl_user values(1, 'admin', SHA('admin'));
+INSERT INTO `tbl_role` VALUES (1,'ADMIN',1);
